f4f1833c9a0be835fc6e46ffd7e108ae5c2a1135
Oliver Butler <dev@oliverbutler.uk>
2024-03-18 08:15:40 +0000

    neotest jest

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -4,21 +4,31 @@ return {
 		"nvim-lua/plenary.nvim",
 		"antoinemadec/FixCursorHold.nvim",
 		"marilari88/neotest-vitest",
-		"nvim-treesitter/nvim-treesitter"
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-neotest/neotest-jest"
 	},
 	config = function()
 		require("neotest").setup({
 			adapters = {
 				require("neotest-vitest"),
+				require('neotest-jest')({
+					jestCommand = "npm test --",
+					jestConfigFile = "jest.config.ts",
+					env = { CI = true },
+					cwd = function(path)
+						return vim.fn.getcwd()
+					end,
+				})
 			}
 		})
 
 		vim.keymap.set("n", "<leader>ts", ":Neotest summary<CR>")
+		vim.keymap.set("n", "<leader>to", ":Neotest output<CR>")
+		vim.keymap.set("n", "<leader>tp", ":Neotest output_panel<CR>")
 
 		vim.keymap.set("n", "<leader>tr", function()
 			require('neotest').run.run()
 		end)
-
 		vim.keymap.set("n", "<leader>tl", function()
 			require('neotest').run.run_last()
 		end)
--- .config/nvim/lua/plugins/notify.lua
+++ .config/nvim/lua/plugins/notify.lua
@@ -3,6 +3,8 @@ return {
 	config = function ()
 		require('notify').setup({
 			  background_colour = "#000000",
+			  opacity = 0.8,
+			  position = "bottom",
 		  })
 	  end
   }
