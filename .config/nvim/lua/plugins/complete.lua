afe25ea015a089269a380de1ed3ea2bae860c9d3
Oliver Butler <dev@oliverbutler.uk>
2024-03-18 22:36:08 +0000

    add relevant snippets for bmo

--- .config/nvim/lua/plugins/complete.lua
+++ .config/nvim/lua/plugins/complete.lua
@@ -11,58 +11,87 @@ return {
 		}
 	},
 	{
-
 		'hrsh7th/nvim-cmp',
 		config = function()
 			local cmp = require('cmp')
-
 			require("luasnip.loaders.from_vscode").lazy_load()
+
+			local function to_pascal_case(file_name)
+				local parts = vim.split(file_name, "[-.]")
+				local pascal_case_parts = {}
+
+				for _, part in ipairs(parts) do
+					if part ~= "ts" then
+						local part_cased = part:gsub("^%l", string.upper)
+						table.insert(pascal_case_parts, part_cased)
+					end
+				end
+				local pascal_name = table.concat(pascal_case_parts, "")
+				return pascal_name
+			end
+
+			-- Add the snippet
+			local ls = require("luasnip")
+			local s = ls.snippet
+			local f = ls.function_node
+			local i = ls.insert_node
+			local t = ls.text_node
+			local fmt = require("luasnip.extras.fmt").fmt
+
+			ls.add_snippets("typescript", {
+				s("inject", fmt(
+				[[
+				import {{ Injectable }} from '@nestjs/common';
+
+				@Injectable()
+				export class {} {{
+					constructor() {{}}
+				}}
+				]],
+				{
+					f(function(_, snip)
+						return to_pascal_case(vim.fn.expand("%:t"))
+					end, {}),
+				}
+				)),
+			})
+
+
+			ls.add_snippets("typescript", {
+				s("controller", fmt(
+				[[
+				import {{ Controller }} from '@nestjs/common';
+				import {{TsRestHandler, tsRestHandler}} from "@ts-rest/nest";
+
+				@Controller()
+				export class {} {{
+					constructor() {{}}
+
+					@TsRestHandler({})
+					async handler() {{
+						return tsRestHandler({}, () => {{
+							{}
+						}})
+					}}
+				}}
+				]],
+				{
+					f(function(_, snip)
+						return to_pascal_case(vim.fn.expand("%:t"))
+					end, {}),
+					i(1, "contract"),
+					t("{1}"),
+					i(0),
+				}
+				)),
+			})
 
 
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-
-
 						local ls = require('luasnip')
-
 						ls.lsp_expand(args.body)
-
-						local s = ls.snippet
-						local i = ls.insert_node
-						local f = ls.function_node
-						local fmt = require("luasnip.extras.fmt").fmt
-
-						-- Function to convert file name to PascalCase
-						local function to_pascal_case(file_name)
-							local parts = vim.split(file_name, "[-.]")
-							local pascal_case_parts = {}
-
-							for _, part in ipairs(parts) do
-								if part ~= "ts" then
-									table.insert(pascal_case_parts, vim.fn.substitute(part, "^.", string.upper, ""))
-								end
-							end
-
-							return table.concat(pascal_case_parts, "")
-						end
-
-
-						ls.add_snippets("typescript", {
-							s("exportclass", fmt(
-							[[
-							export class {} {{
-								constructor() {{}}
-							}}
-							]],
-							{
-								f(function(_, snip)
-									return to_pascal_case(vim.fn.expand("%:t"))
-								end, {}),
-							}
-							)),
-						})
-
 					end,
 				},
 				window = {
