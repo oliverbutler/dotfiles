4cca0900ef3967b962a9196e7881bd0a58a47d4e
oliverbutler <dev@oliverbutler.uk>
2024-03-18 18:25:37 +0000

    add prmake

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -67,3 +67,37 @@ set -gx PATH "$PNPM_HOME" $PATH
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+function prmake
+	# Get the current branch name as the default PR title
+	set branch_name (git symbolic-ref --short HEAD)
+
+	# If an argument is provided, it's used as the PR title, not the branch name
+	if set -q argv[1]
+		set pr_title $argv[1]
+	else
+		# If no PR title is provided, use the current branch name with modifications as the title
+		set pr_title $branch_name
+	end
+
+	# Apply transformations to the branch name for the PR title
+	set pr_title (string replace -r '^feature\/' 'feat: ' $pr_title)
+	set pr_title (string replace -r '^bugfix\/' 'fix: ' $pr_title)
+	set pr_title (string replace -r '^documentation\/' 'docs: ' $pr_title)
+	set pr_title (string replace -r '^style\/' 'style: ' $pr_title)
+	set pr_title (string replace -r '^refactoring\/' 'refactor: ' $pr_title)
+	set pr_title (string replace -r '^testing\/' 'test: ' $pr_title)
+	set pr_title (string replace -r '^chore\/' 'chore: ' $pr_title)
+
+	# Global replacement of hyphens with spaces in the title
+	set pr_title (string replace -a '-' ' ' $pr_title)
+
+	# Push the current branch; no branch creation or checkout is performed
+	git push --set-upstream origin $branch_name
+
+	# Create a PR with the specified or modified title
+	gh pr create --title "$pr_title" --fill
+
+	# Open the newly created PR in the web browser
+	gh pr view --web
+end
