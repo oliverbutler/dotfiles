4dc7a9554988e83aacdf0a5647639c4fcbcb12a0
oliverbutler <dev@oliverbutler.uk>
2024-03-22 08:16:15 +0000

    perf: escape time

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 	{
 		'nvim-treesitter/nvim-treesitter',
 		config = function()
-			require'nvim-treesitter.configs'.setup {
+			require 'nvim-treesitter.configs'.setup {
 
 				-- A list of parser names, or "all" (the five listed parsers should always be installed)
 				ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "typescript" },
