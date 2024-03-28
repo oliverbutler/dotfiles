61e5000eae9a60b1a0b082d03a60c45f5a3929f2
oliverbutler <dev@oliverbutler.uk>
2024-03-28 07:56:16 +0000

    feat: disable ts function calls complete

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -90,7 +90,7 @@ return {
           -- https://github.com/microsoft/TypeScript/blob/3c221fc086be52b19801f6e8d82596d04607ede6/src/compiler/utilitiesPublic.ts#L620
           tsserver_locale = "en",
           -- mirror of VSCode's `typescript.suggest.completeFunctionCalls`
-          complete_function_calls = true,
+          complete_function_calls = false,
           include_completions_with_insert_text = true,
           -- CodeLens
           -- WARNING: Experimental feature also in VSCode, because it might hit performance of server.
