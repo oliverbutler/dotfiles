1f986e5acd925a6d48939ba3de5672d140f91a90
Oliver Butler <dev@oliverbutler.uk>
2024-04-11 20:12:40 +0100

    feat: kitty

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -92,7 +92,7 @@ return {
         end,
         settings = {
           -- spawn additional tsserver instance to calculate diagnostics on it
-          separate_diagnostic_server = false, -- testing
+          separate_diagnostic_server = true, -- testing
           -- "change"|"insert_leave" determine when the client asks the server about diagnostic
           publish_diagnostic_on = "insert_leave",
           -- array of strings("fix_all"|"add_missing_imports"|"remove_unused"|
