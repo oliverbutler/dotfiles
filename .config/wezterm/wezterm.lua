0ea2393370561246f75187047fcc52f10f73f4fa
Oliver Butler <dev@oliverbutler.uk>
2024-04-13 21:15:03 +0100

    feat: improve rendering with wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,24 +2,30 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Mocha"
-config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.9
-config.macos_window_background_blur = 15
+-- Theme
+config.color_scheme = "catppuccin-mocha"
+config.enable_tab_bar = false
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
 
 -- Fonts
+-- Emojis 🤣💀👻✅🐳
 config.font_size = 12.0
+config.foreground_text_hsb = {
+  hue = 1.0,
+  saturation = 0.95,
+  brightness = 1.0,
+}
 config.font = wezterm.font_with_fallback({
   {
     family = "JetBrains Mono",
-    weight = "DemiBold",
   },
-  { family = "Apple Color Emoji", assume_emoji_presentation = true },
+  { family = "Apple Color Emoji", assume_emoji_presentation = true, scale = 1.2 },
 })
 
+-- Window
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 15
 config.adjust_window_size_when_changing_font_size = false
-
 config.window_padding = {
   left = 0,
   right = 0,
