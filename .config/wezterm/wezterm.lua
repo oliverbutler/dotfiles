316de27eac931ead23fff09e9dae60517e542ea4
Oliver Butler <dev@oliverbutler.uk>
2024-04-13 09:18:14 +0100

    feat: finish wezterm setup

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,7 +3,9 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Mocha"
-
 config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 15
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
 
 return config
