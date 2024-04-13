d2c3fd3c4406537443262ce51f9752bc446a5cf1
Oliver Butler <dev@oliverbutler.uk>
2024-04-13 09:23:33 +0100

    feat: add window customise

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,4 +8,11 @@ config.window_background_opacity = 0.9
 config.macos_window_background_blur = 15
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
 
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
 return config
