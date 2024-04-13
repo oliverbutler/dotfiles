9a903d9052971168fd93a956b9348787fd029f26
Oliver Butler <dev@oliverbutler.uk>
2024-04-13 17:46:26 +0100

    refactor: fterm reusable logic

--- .config/nvim/lua/plugins/fterm.lua
+++ .config/nvim/lua/plugins/fterm.lua
@@ -3,55 +3,38 @@ return {
   config = function()
     local fterm = require("FTerm")
 
-    fterm.setup({
-      dimensions = {
-        height = 0.8,
-        width = 0.8,
+    local programs = {
+      {
+        cmd = nil,
+        key = "<leader>tt",
       },
-      border = "single",
-    })
-
-    vim.keymap.set("n", "<leader>tt", function()
-      require("FTerm").toggle()
-    end, { noremap = true, silent = true, desc = "Toggle [T]erm" })
-
-    local lazygitui = fterm:new({
-      cmd = "lazygit",
-      dimensions = {
-        height = 0.9,
-        width = 0.9,
+      {
+        cmd = "lazygit",
+        key = "<leader>gl",
       },
-      border = "single",
-    })
-
-    vim.keymap.set("n", "<leader>gl", function()
-      lazygitui:toggle()
-    end)
-
-    local lazy_home_git_ui = fterm:new({
-      cmd = "yal",
-      dimensions = {
-        height = 0.9,
-        width = 0.9,
+      {
+        cmd = "lazydocker",
+        key = "<leader>ld",
       },
-      border = "single",
-    })
-
-    vim.keymap.set("n", "<leader>g;", function()
-      lazy_home_git_ui:toggle()
-    end)
-
-    local lazy_docker_ui = fterm:new({
-      cmd = "lazydocker",
-      dimensions = {
-        height = 0.9,
-        width = 0.9,
+      {
+        cmd = "cd ~/projects/gosuite && go run main.go",
+        key = "<leader>gs",
       },
-      border = "single",
-    })
+    }
 
-    vim.keymap.set("n", "<leader>ld", function()
-      lazy_docker_ui:toggle()
-    end)
+    for _, program in ipairs(programs) do
+      local fterm_instance = fterm:new({
+        cmd = program.cmd,
+        dimensions = {
+          height = 0.9,
+          width = 0.9,
+        },
+        border = "single",
+      })
+
+      vim.keymap.set("n", program.key, function()
+        fterm_instance:toggle()
+      end)
+    end
   end,
 }
