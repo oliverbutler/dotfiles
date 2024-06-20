eb331060954ba2a144719f6fd18f7d382ea9f305
Oliver Butler <dev@oliverbutler.uk>
2024-06-20 14:00:32 +0100

    feat: trouble and quickfix improvements

--- .config/nvim/lua/plugins/bqf.lua
+++ .config/nvim/lua/plugins/bqf.lua
@@ -1,9 +1,18 @@
 return {
   "kevinhwang91/nvim-bqf",
+  keys = {
+    {
+      "<leader>q",
+      ":copen<CR>",
+      desc = "Open quickfix window",
+    },
+    {
+      "<leader>Q",
+      ":cclose<CR>",
+      desc = "Close quickfix window",
+    },
+  },
   config = function()
     require("bqf").setup({})
-
-    vim.keymap.set("n", "<leader>q", ":copen<CR>", { noremap = true, silent = true, desc = "Open quickfix window" })
-    vim.keymap.set("n", "<leader>Q", ":cclose<CR>", { noremap = true, silent = true, desc = "Close quickfix window" })
   end,
 }
