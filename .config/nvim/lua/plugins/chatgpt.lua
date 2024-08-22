8e17c33acaa6ee441625cbd6e05abda534d2415f
Oliver Butler <dev@oliverbutler.uk>
2024-08-22 20:45:06 +0100

    pause usage as not using them

--- .config/nvim/lua/plugins/chatgpt.lua
+++ .config/nvim/lua/plugins/chatgpt.lua
@@ -1,18 +1,18 @@
 return {
-  "jackMort/ChatGPT.nvim",
-  cmd = { "ChatGPT", "ChatGPTEditWithInstructions" },
-  config = function()
-    local home = os.getenv("HOME")
-    local api_key_cmd = home .. "/.config/nvim/lua/plugins/get-openapi-key.sh"
-
-    require("chatgpt").setup({
-      api_key_cmd = api_key_cmd,
-    })
-  end,
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "nvim-lua/plenary.nvim",
-    "folke/trouble.nvim",
-    "nvim-telescope/telescope.nvim",
-  },
+  -- "jackMort/ChatGPT.nvim",
+  -- cmd = { "ChatGPT", "ChatGPTEditWithInstructions" },
+  -- config = function()
+  --   local home = os.getenv("HOME")
+  --   local api_key_cmd = home .. "/.config/nvim/lua/plugins/get-openapi-key.sh"
+  --
+  --   require("chatgpt").setup({
+  --     api_key_cmd = api_key_cmd,
+  --   })
+  -- end,
+  -- dependencies = {
+  --   "MunifTanjim/nui.nvim",
+  --   "nvim-lua/plenary.nvim",
+  --   "folke/trouble.nvim",
+  --   "nvim-telescope/telescope.nvim",
+  -- },
 }
