5ea174fb79b2d190b9436e55b323fff7bd804437
Oliver Butler <dev@oliverbutler.uk>
2024-09-22 10:37:22 +0100

    refactor: pull out nivida

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -15,6 +15,7 @@ in
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./nvidia.nix
     ];
 
   # Bootloader.
@@ -179,67 +180,6 @@ in
 
   # Gaming
 
-  hardware.opengl = { 
-    enable = true; 
-  }; 
-
-  services.xserver.videoDrivers = [ "nvidia" ]; 
-
-  hardware.nvidia = {
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    # NOTE: This fixed resume for sleep for me on olly-desktop
-    powerManagement.enable = true;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    # package = config.boot.kernelPackages.nvidiaPackages.beta;
-
-# NOTE: This WORKED, but i wanna try later drivers.
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-  version = "555.58";
-
-  sha256_64bit = "sha256-bXvcXkg2kQZuCNKRZM5QoTaTjF4l2TtrsKUvyicj5ew=";
-  sha256_aarch64 = lib.fakeSha256;
-  openSha256 = lib.fakeSha256;
-  settingsSha256 = "sha256-vWnrXlBCb3K5uVkDFmJDVq51wrCoqgPF03lSjZOuU8M=";
-  persistencedSha256 = lib.fakeSha256;
-};
-
-#
-#     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-#   version = "560.35.03";
-#
-#   sha256_64bit = "sha256-8pMskvrdQ8WyNBvkU/xPc/CtcYXCa7ekP73oGuKfH+M=";
-#   sha256_aarch64 = lib.fakeSha256;
-#   openSha256 = lib.fakeSha256;
-#   settingsSha256 = "sha256-kQsvDgnxis9ANFmwIwB7HX5MkIAcpEEAHc8IBOLdXvk=";
-#   persistencedSha256 = lib.fakeSha256;
-# };
-
-
-  };
 
   programs.steam.enable = true;
   programs.steam.gamescopeSession.enable = true;
