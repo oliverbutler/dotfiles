4ac21f51625e2671cf291f0bc139c111bf53312e
Oliver Butler <dev@oliverbutler.uk>
2025-02-16 17:40:45 +0000

    fix: disable ctrl+k

--- .config/nvim/lua/plugins/windows.lua
+++ .config/nvim/lua/plugins/windows.lua
@@ -4,6 +4,28 @@ return {
     "anuvyklack/middleclass",
     "anuvyklack/animation.nvim",
   },
+  keys = {
+    {
+      "<C-w>z",
+      "<cmd>WindowsMaximize<cr>",
+      desc = "Maximize window",
+    },
+    {
+      "<C-w>_",
+      "<cmd>WindowsMaximizeVertically<cr>",
+      desc = "Maximize window vertically",
+    },
+    {
+      "<C-w>|",
+      "<cmd>WindowsMaximizeHorizontally<cr>",
+      desc = "Maximize window horizontally",
+    },
+    {
+      "<C-w>=",
+      "<cmd>WindowsEqualize<cr>",
+      desc = "Equalize window",
+    },
+  },
   config = function()
     vim.o.winwidth = 10
     vim.o.winminwidth = 10
