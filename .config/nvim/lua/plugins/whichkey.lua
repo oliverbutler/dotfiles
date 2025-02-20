c42f9bcf933eafd6f00934c667e9c92c5c4aa447
Oliver Butler <dev@oliverbutler.uk>
2025-02-20 17:47:16 +0000

    feat: wip mini surround

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -7,16 +7,6 @@ return {
   config = function(_, opts)
     local wk = require("which-key")
     wk.setup(opts)
-    wk.register({
-      { "<leader>c", group = "[C]ode" },
-      { "<leader>c_", hidden = true },
-      { "<leader>d", group = "[D]ocument" },
-      { "<leader>d_", hidden = true },
-      { "<leader>r", group = "[R]ename" },
-      { "<leader>r_", hidden = true },
-      { "<leader>s", group = "[S]earch" },
-      { "<leader>s_", hidden = true },
-    })
   end,
   opts = {
     -- Add any which-key configuration options here
